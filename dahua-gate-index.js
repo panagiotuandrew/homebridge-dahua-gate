f02300ac4650e20cc9581dc34415274aefb07343
Andrew Panagiotou <panagiotouandrew@icloud.com>
2026-04-20 23:39:05 +0300

    Refactor Dahua Gate plugin to use dynamic platform

--- dahua-gate-index.js
+++ dahua-gate-index.js
@@ -3,34 +3,70 @@
 const axios = require("axios");
 const crypto = require("crypto");
 
-module.exports = (homebridge) => {
-  homebridge.registerAccessory(
-    "homebridge-dahua-gate",
+module.exports = (api) => {
+  api.registerDynamicPlatformPlugin(
     "Dahua Gate",
-    DahuaGateRelease
+    DahuaGatePlatform
   );
 };
 
-class DahuaGateRelease {
+class DahuaGatePlatform {
   constructor(log, config, api) {
-    this.Service         = api.hap.Service;
-    this.Characteristic  = api.hap.Characteristic;
-    this.log             = log;
-    this.name            = config.name;
-    this.ip              = config.ip;
-    this.username        = config.username;
-    this.password        = config.password;
-    this.pollInterval    = config.pollInterval    ?? 60000;
-    this.verboseLogging  = config.verboseLogging  ?? false;
-    this.fallbackRetries = config.fallbackRetries ?? 1;
-    this.fallbackDelay   = config.fallbackDelay   ?? 5000;
-    this.autoLockTime    = config.autoLockTime    ?? 0;
-    this.autoLockUnit    = config.autoLockUnit    ?? "seconds";
-    this._faultState     = this.Characteristic.StatusFault.NO_FAULT;
+    this.log        = log;
+    this.config     = config;
+    this.api        = api;
+    this.accessories = [];
 
+    this.api.on("didFinishLaunching", () => {
+      this.discoverDevices();
+    });
+  }
+
+  configureAccessory(accessory) {
+    this.accessories.push(accessory);
+  }
+
+  discoverDevices() {
+    const devices = this.config.devices || [];
+
+    for (const device of devices) {
+      const uuid = this.api.hap.uuid.generate(device.name);
+      const existingAccessory = this.accessories.find(a => a.UUID === uuid);
+
+      if (existingAccessory) {
+        this.log.info(`Restoring accessory from cache: ${existingAccessory.displayName}`);
+        new DahuaGateAccessory(this.log, existingAccessory, this.api, device);
+      } else {
+        this.log.info(`Adding new accessory: ${device.name}`);
+        const accessory = new this.api.platformAccessory(device.name, uuid);
+        new DahuaGateAccessory(this.log, accessory, this.api, device);
+        this.api.registerPlatformAccessories("homebridge-dahua-gate", "Dahua Gate", [accessory]);
+      }
+    }
+  }
+}
+
+class DahuaGateAccessory {
+  constructor(log, accessory, api, device) {
+    this.Service        = api.hap.Service;
+    this.Characteristic = api.hap.Characteristic;
+    this.log            = log;
+    this.accessory      = accessory;
+    this.name           = device.name;
+    this.ip             = device.ip;
+    this.username       = device.username;
+    this.password       = device.password;
+    this.pollInterval   = device.pollInterval   ?? 60000;
+    this.verboseLogging = device.verboseLogging  ?? false;
+    this.fallbackRetries = device.fallbackRetries ?? 1;
+    this.fallbackDelay  = device.fallbackDelay   ?? 5000;
+    this.autoLockTime   = device.autoLockTime    ?? 0;
+    this.autoLockUnit   = device.autoLockUnit    ?? "seconds";
+    this._faultState    = this.Characteristic.StatusFault.NO_FAULT;
+
+  this.service = this.accessory.getService(this.Service.LockMechanism)
+    || this.accessory.addService(this.Service.LockMechanism);
     this.axios = axios;
-
-    this.service = new this.Service.LockMechanism(this.name);
 
     this.service
       .getCharacteristic(this.Characteristic.StatusFault)
@@ -46,10 +82,6 @@ class DahuaGateRelease {
       .onSet(this.handleSetTargetState.bind(this));
 
     this.startPolling();
-  }
-
-  getServices() {
-    return [this.service];
   }
 
   async handleGetCurrentState() {
